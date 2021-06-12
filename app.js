f384b18d09a1b54aed49b1d335786233c7f27f09
ntziris <ntziris@gmail.com>
2021-06-12 16:37:37 +0300

    added dotenv functionality

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env' });
 
 const app = express();
 
 // Start DB
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'hotel-DBMS',
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE,
 });
 
 db.connect((error) => {

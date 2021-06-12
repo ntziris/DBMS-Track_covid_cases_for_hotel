d6126801fe22c7078d41043e995e43f64def70ab
ntziris <ntziris@gmail.com>
2021-06-12 18:27:18 +0300

    installed bcrypt, cookie-parser and jsonwebtoken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,32 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE,
+});
+
 exports.register = (req, res) => {
   console.log(req.body);
-  res.send("Form submitted");
-}
+
+  const { name, email, password, passwordConfirm } = req.body;
+
+  db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {
+    if (error) {
+      console.log(error);
+    }
+
+    if (results.length > 0) {
+      return res.render('register', {
+        message: 'That email is already in use.'
+      })
+    } else if (password !== passwordConfirm) {
+      return res.render('register', {
+        message: "Passwords don't match."
+      });
+    }
+  });
+
+  res.send('Form submitted');
+};

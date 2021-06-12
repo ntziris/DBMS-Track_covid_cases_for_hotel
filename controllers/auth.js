a81313ac668e1c5168de1e72eeb4c2e5b39fb299
ntziris <ntziris@gmail.com>
2021-06-12 18:40:13 +0300

    register new account functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const db = mysql.createConnection({
   host: process.env.DATABASE_HOST,
@@ -12,21 +14,43 @@ exports.register = (req, res) => {
 
   const { name, email, password, passwordConfirm } = req.body;
 
-  db.query('SELECT email FROM users WHERE email = ?', [email], (error, results) => {
-    if (error) {
-      console.log(error);
-    }
+  db.query(
+    'SELECT email FROM users WHERE email = ?',
+    [email],
+    async (error, results) => {
+      if (error) {
+        console.log(error);
+      }
 
-    if (results.length > 0) {
-      return res.render('register', {
-        message: 'That email is already in use.'
-      })
-    } else if (password !== passwordConfirm) {
-      return res.render('register', {
-        message: "Passwords don't match."
+      if (results.length > 0) {
+        return res.render('register', {
+          message: 'That email is already in use.',
+        });
+      } else if (password !== passwordConfirm) {
+        return res.render('register', {
+          message: "Passwords don't match.",
+        });
+      }
+
+      let hashedPassword = await bcrypt.hash(password, 8);
+      console.log(hashedPassword);
+
+      db.query('INSERT INTO users SET ?', {
+        name: name,
+        email: email,
+        password: hashedPassword,
+      }, (error, results) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(results);
+          return res.render('register', {
+            message: "Account Created!",
+          });
+        }
       });
     }
-  });
+  );
 
-  res.send('Form submitted');
+  // res.send('Form submitted');
 };
